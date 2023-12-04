94494f3d8f17b0d969488a740e2917650b518d44
tellybah2208 <thiernobah2208@gmail.com>
2023-12-04 16:45:21 +0100

    Initial Commit

---
+++ test_projet/test_projetApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_projetApp.swift
+//  test_projet
+//
+//  Created by Telly BAH on 04/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct test_projetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
